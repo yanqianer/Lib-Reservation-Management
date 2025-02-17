9168fdf73e023f14f54991e1d69ac7bec467afff
彭于晏在线学java <2091764628@qq.com>
2025-02-17 18:58:33 +0800

    second

--- env.d.ts
+++ env.d.ts
@@ -3,4 +3,23 @@ declare module '*.vue' {
     import { ComponentOptions } from 'vue'
     const componentOptions: ComponentOptions
     export default componentOptions
+    interface ImportMetaEnv {
+        readonly VITE_PORT: string;
+        readonly VITE_API_BASE_URL: string;
+        // 可以在这里继续添加你的环境变量
+      }
+      
+      interface ImportMeta {
+        readonly env: ImportMetaEnv;
+      }
+    
 }
+interface ImportMetaEnv {
+    readonly VITE_PORT: string;
+    readonly VITE_API_BASE_URL: string;
+    // 可以在这里继续添加你的环境变量
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({mode})=>{
+  const env = loadEnv(mode, process.cwd(), ''); // 指定加载前缀为空字符串
+  
+
+  return{
+
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    server:{
+      port: Number(env.VITE_PORT) || 5173,
     },
-  },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+    },
+  }
+ 
 })
